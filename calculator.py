6282f3ce03a1323060813db71f57f71c367e15b1
kalyaniavni9-lang <kalyaniavni9@gmail.com>
2026-05-01 20:44:08 +0530

    Add basic calculator functionality in calculator.py

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input("enter first number"))
+b = int(input("enter second number"))
+
+print("Sum:",  a + b )
+print("Difference:" , a - b)
+print("Multiplication:", a * b)      
+
+
+if b == 0:
+  print("Cannot divide by zero")
+else:
+  print("divison:", a/b)
+      
+        
